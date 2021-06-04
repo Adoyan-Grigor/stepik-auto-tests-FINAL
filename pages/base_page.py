273604ef5b4e7cb09d0d47f4631dc35eb7a52510
jitechpc3 <you@example.com>
2021-06-04 15:06:05 +0400

    stepik

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from selenium import webdriver
+browser = webdriver.Chrome()
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self): 
+        self.browser.get(self.url)
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import time
+class MainPage(BasePage):
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
